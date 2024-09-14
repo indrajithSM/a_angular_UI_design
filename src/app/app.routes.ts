ce88418090dc3eae83f4e7b367e432405339a603
indrajith.sabapathy <indrajith.sabapathy@solverminds.sg>
2024-09-14 14:51:15 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
+  },
+  {
+    path:'',
+    redirectTo:'dashboard',
+    pathMatch:'full'
+  }
+];
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { PrimengComponentsModule } from './primeng-components/primeng-components.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ]
+})
+export class CoreModule { }
