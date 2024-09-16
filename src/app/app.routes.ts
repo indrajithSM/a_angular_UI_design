1d0cac1a9aff1185f0eea9b9fe6093686dd1c1c6
indrajith1116 <indrasabapathi12@gmail.com>
2024-09-16 21:23:38 +0530

    latest Changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,26 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
 
 export const routes: Routes = [
   {
-    path: 'dashboard',
-    component: DashboardHomeComponent,
+    path: 'app',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardHomeComponent,
+      },
+      {
+        path:'',
+        redirectTo:'dashboard',
+        pathMatch:'full'
+      }
+    ],
   },
   {
-    path:'',
-    redirectTo:'dashboard',
-    pathMatch:'full'
-  }
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full',
+  },
 ];
